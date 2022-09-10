c0e4ffc0c377d8f1f9b0cb60c4c3c8f66a54e869
Vitalik Kolesnikov <VitaliKolesnikov22@gmail.com>
2022-09-10 13:21:06 +0300

    add: Добавил функцию getCreateList

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@
 const form = document.querySelector('.form');
 const listInput = document.querySelector('#list-input');
 const listButton = document.querySelector('#list-button');
+const container = document.querySelector('.container');
 
 console.log(listButton);
 console.log(listInput);
 
+//Меняю цвет кнопки и класс на button_active
 listInput.addEventListener('input', () => listButton.classList[listInput.value ? 'add' : 'remove']('form__button_active'));
+
+
 
 const lists = [];
 
@@ -19,4 +23,26 @@ function getRandomId() {
     return id
 }
 
+listButton.addEventListener('click', getCreateList);
 
+
+//Функции
+function getCreateList() {
+    //Создаю новый блок list
+    const id = getRandomId()
+    const list = document.createElement('div');
+    list.className = 'main-list';
+    list.id = id
+    list.textContent = listInput.value
+    lists.push({
+        id,
+        title: listInput.value,
+        items: []
+    })
+
+    container.appendChild(list)
+
+    //Создаю новый блок 
+
+
+}
